3f3c8c86667337181c54a1c48fb4ac3918f15063
Ali <ali.m.198384@gmail.com>
2021-05-14 10:40:57 +0430

    first commit

--- app/src/main/java/com/ali/clquestion/MainActivity.kt
+++ app/src/main/java/com/ali/clquestion/MainActivity.kt
@@ -2,11 +2,6 @@ package com.ali.clquestion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import ir.map.servicesdk.MapService
-import ir.map.servicesdk.ResponseListener
-import ir.map.servicesdk.model.base.MapirError
-import ir.map.servicesdk.response.ReverseGeoCodeResponse
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
