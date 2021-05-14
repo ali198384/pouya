b04a90d97110a6cfe259787c990cd0142f24812e
Ali <ali.m.198384@gmail.com>
2021-05-14 10:49:16 +0430

    add function

--- app/src/main/java/com/ali/clquestion/MainActivity.kt
+++ app/src/main/java/com/ali/clquestion/MainActivity.kt
@@ -2,14 +2,16 @@ package com.ali.clquestion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //new line
+
     }
     fun myfunction() {
-
+        Log.v("TAG1", "Hi Ali")
     }
 }
